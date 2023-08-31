2509320afbe54a6693bff3e8cd575d9ca4137ce0
ruqwk99 <140777537+ruqwk99@users.noreply.github.com>
2023-08-31 22:42:34 +0900

    Update app.js

--- app.js
+++ app.js
@@ -10,16 +10,12 @@ var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 
 var app = express();
-const port = 3334;
-app.use(cors());
+//const port = 3333;
+app.use(cors()); //모든통신허용
 
 app.get('/api/data', (req, res) => {
   const data = { message: 'Hello from Node.js server!' };
   res.json(data);
-});
-
-app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
 });
 
 // view engine setup
