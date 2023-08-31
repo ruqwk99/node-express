dae1c56fc106eb5b415990722b64c6cb40bff3a4
ruqwk99 <140777537+ruqwk99@users.noreply.github.com>
2023-08-31 21:37:18 +0900

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const nunjucks = require('nunjucks');
-const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 
 var app = express();
-app.use(cors());
 const port = 3334;
 
 app.get('/api/data', (req, res) => {
